2c5813041f2480b7f01f189ef470c22832b19bd2
Brian Chiang <brian_chiang@chief.com.tw>
2023-03-01 17:58:49 +0800

    fix search form

--- static/data_filter.js
+++ static/data_filter.js
@@ -1,24 +1,24 @@
-$(document).ready(function () {
-    // 過濾型號選單
-    $('#brand, #model, #importance').change(function () {
-        var brand = $('#brand').val();
-        var model = $('#model').val();
-        var importance = $('#importance').val();
-        var url = '/';
-        if (brand) {
-            url += '?brand=' + encodeURIComponent(brand);
-        }
-        if (model) {
-            url += (brand ? '&' : '?') + 'model=' + encodeURIComponent(model);
-        }
-        if (importance) {
-            url += (brand || model ? '&' : '?') + 'importance=' + encodeURIComponent(importance);
-        }
-        $.get(url, function (data) {
-            $('#drivers-table').html($(data).find('#drivers-table').html());
-        });
-    });
-});
+// $(document).ready(function () {
+//     // 過濾型號選單
+//     $('#brand, #model, #importance').change(function () {
+//         var brand = $('#brand').val();
+//         var model = $('#model').val();
+//         var importance = $('#importance').val();
+//         var url = '/';
+//         if (brand) {
+//             url += '?brand=' + encodeURIComponent(brand);
+//         }
+//         if (model) {
+//             url += (brand ? '&' : '?') + 'model=' + encodeURIComponent(model);
+//         }
+//         if (importance) {
+//             url += (brand || model ? '&' : '?') + 'importance=' + encodeURIComponent(importance);
+//         }
+//         $.get(url, function (data) {
+//             $('#drivers-table').html($(data).find('#drivers-table').html());
+//         });
+//     });
+// });
 
 $(function () {
     // 當品牌選單改變時，過濾型號選單
@@ -67,16 +67,47 @@ function sortTable(columnIndex) {
     sortDirection = 1 - sortDirection; // 切換排序方向
 }
 
-$(function () {
-    var delayTimer;
-    $('#search-input').on('input', function () {
-        clearTimeout(delayTimer);
-        var search = $(this).val();
-        delayTimer = setTimeout(function () {
-            // 發起帶有 search 參數的 GET 請求
-            $.get('/', { 'search': search }, function (data) {
-                $('#drivers-table').html($(data).find('#drivers-table').html());
-            });
-        }, 750);
+// $(function () {
+//     var delayTimer;
+//     $('#search-input').on('input', function () {
+//         clearTimeout(delayTimer);
+//         var search = $(this).val();
+//         delayTimer = setTimeout(function () {
+//             // 發起帶有 search 參數的 GET 請求
+//             $.get('/', { 'search': search }, function (data) {
+//                 $('#drivers-table').html($(data).find('#drivers-table').html());
+//             });
+//         }, 750);
+//     });
+// });
+
+$(document).ready(function () {
+    $('#brand, #model, #importance, #search-input').change(function () {
+        var brand = $('#brand').val();
+        var model = $('#model').val();
+        var importance = $('#importance').val();
+        var search = $('#search-input').val();
+        var url = '/';
+        var params = {};
+        if (brand) {
+            params.brand = brand;
+        }
+        if (model) {
+            params.model = model;
+        }
+        if (importance) {
+            params.importance = importance;
+        }
+        if (search) {
+            params.search = search;
+        }
+        if (Object.keys(params).length) {
+            url += '?' + $.param(params);
+        }
+        $.get(url, function (data) {
+            $('#drivers-table').html($(data).find('#drivers-table').html());
+        });
     });
 });
+
+
