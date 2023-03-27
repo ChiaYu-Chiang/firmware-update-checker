e809f2b390acd508951f55cdc0f433fe6d8da26a
Brian Chiang <brian_chiang@chief.com.tw>
2023-03-27 10:34:59 +0800

    set web server host ip default to 0.0.0.0

--- run_waitress.py
+++ run_waitress.py
@@ -1,5 +1,8 @@
 from waitress import serve
 from app import app
+import os
 
 if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=5001)
+    # if use reverse proxy, set HOST to localhost
+    host = os.environ.get("HOST", "0.0.0.0")
+    serve(app, host=host, port=5001)
